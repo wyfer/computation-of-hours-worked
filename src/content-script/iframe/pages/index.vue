f7f41cacf47876e2d1cc6a9cbdcfbcc34b03184b
王永锋 <yongfeng.wang@uni-orange.com>
2024-07-10 14:47:03 +0800

    feat✨: skip content-script

--- src/content-script/iframe/pages/index.vue
+++ src/content-script/iframe/pages/index.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1 class="text-red-800 font-bold text-xl">
-      Content Script Iframe
-    </h1>
-    <button class="btn btn-primary">
-      Sample Daisy UI Button
-    </button>
-  </div>
+  <div />
 </template>
 
 <style scoped></style>
--- src/content-script/index.ts
+++ src/content-script/index.ts
@@ -1,18 +1,18 @@
-import './index.scss'
+// import './index.scss'
 
-const src = chrome.runtime.getURL('src/content-script/iframe/index.html')
+// const src = chrome.runtime.getURL('src/content-script/iframe/index.html')
 
-const iframe = new DOMParser().parseFromString(
-  `<iframe class="crx-iframe" src="${src}"></iframe>`,
-  'text/html'
-).body.firstElementChild
+// const iframe = new DOMParser().parseFromString(
+//   `<iframe class="crx-iframe" src="${src}"></iframe>`,
+//   'text/html'
+// ).body.firstElementChild
 
-if (iframe) {
-  document.body?.append(iframe)
-}
+// if (iframe) {
+//   document.body?.append(iframe)
+// }
 
-self.onerror = function (message, source, lineno, colno, error) {
-  console.info(
-    `Error: ${message}\nSource: ${source}\nLine: ${lineno}\nColumn: ${colno}\nError object: ${error}`
-  )
-}
+// self.onerror = function (message, source, lineno, colno, error) {
+//   console.info(
+//     `Error: ${message}\nSource: ${source}\nLine: ${lineno}\nColumn: ${colno}\nError object: ${error}`
+//   )
+// }
